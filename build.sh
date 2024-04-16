ef8d43629705eed686f522939d77b32805ddc2a9
unknown <pkmniako@gmail.com>
2024-04-16 12:09:34 +0200

    Simple build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+# Get version from assembly file
+ASSEMBLY_INFO_PATH="src/OtherWorldsProgram/Properties/AssemblyInfo.cs"
+REGEX_VERSION_PARSING="\[assembly: KSPAssembly\(\"(.*)\",\s?([0-9]*),\s?([0-9]*),\s?([0-9]*)\)\]"
+REGEX_VERSION_PARSING="\[assembly: KSPAssembly\(\"(.*)\", ([0-9]*), ([0-9]*), ([0-9]*)"
+
+while IFR= read -r line
+do
+	if [[ $line =~ $REGEX_VERSION_PARSING ]]
+	then 
+		V0="${BASH_REMATCH[2]}"
+		V1="${BASH_REMATCH[3]}"
+		V2="${BASH_REMATCH[4]}"
+		break
+	fi
+done < "$ASSEMBLY_INFO_PATH"
+
+echo "Version found: $V0.$V1.$V2"
+
+# Zip
+echo "Creating zip file, please wait..."
+mkdir -p builds
+cd gamedata
+zip -r ../builds/Other_Worlds_$V0.$V1.$V2.zip *
+
+# Done
+echo "Done!"
